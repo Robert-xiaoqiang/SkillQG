5348846b859fe833905c668c30a9d767aa8f6586
xiaoqiang <3160101819@zju.edu.cn>
2022-06-11 00:57:16 -0400

    Update::BertForMultiLabelSequenceClassification

---
+++ skillqgpackage/data/FairytaleQAMLCLSDataset.py
@@ -0,0 +1,145 @@
+import torch
+from torch.utils.data import Dataset
+import pytorch_lightning as pl
+
+import os
+import json
+import csv
+
+class FairytaleQAMLCLSDataset(Dataset):
+    def __init__(self, config, split_set, tokenizer):
+        super().__init__()
+        self.config = config
+        self.split_set = split_set
+        self.tokenizer = tokenizer
+
+        # this in-memory construction on CPU is infeasible for longer sequence
+        self.parse_and_build()
+
+    def parse_and_build(self):
+        assert self.config.TRAIN.DATASET_FILENAME == self.config.VAL.DATASET_FILENAME == self.config.TEST.DATASET_FILENAME, 'FairytaleQA directory parsing error'
+        root_directory = self.config.TRAIN.DATASET_FILENAME
+        context_root_directory = os.path.join(root_directory, 'section-stories')
+        question_root_directory = os.path.join(root_directory, 'questions')
+        
+        assert self.split_set in { 'train', 'val', 'test' }, 'FairytaleQA splits parsing error'
+
+        context_directory = os.path.join(context_root_directory, self.split_set)
+        question_directory = os.path.join(question_root_directory, self.split_set)
+
+        # # convert
+        self.contexts = [ ]
+        self.questions = [ ]
+        self.answers = [ ]
+        self.reasoning_skills = [ ]
+        input_contexts = [ ]
+        self.qids = [ ]
+
+        '''
+            0: remember
+            1: understand
+            2: apply
+            3: analyze
+            4: create
+            5: evaluate
+        '''
+        self.skill2cls = {
+            'character': 0,
+            'setting': 0,
+            'feeling': 1,
+            'action': 2,
+            'causal relationship': 3,
+            'outcome resolution': 3,
+            'prediction': 4
+        }
+
+        def csv2(section_id, context_list):
+            '''
+                section_id: str or list[str], count from 1
+                context_list: list of <section_id(str), its text(str)> pairs, removing its original header, count from 0 (its index + 1 == section_id)
+            '''
+            section_id_list = [ int(section_id) ] if section_id.find(',') == -1 else list(map(lambda s: int(s), section_id.split(',')))
+            section_text_list = [ ]
+            for sid in section_id_list:
+                section_id_str, section_text = context_list[sid - 1] # list of strs
+                section_text_list.append(section_text.strip())
+
+            ret = ' '.join(section_text_list)
+            
+            return ret
+
+        for question_file in os.listdir(question_directory):
+            question_main_filename = os.path.splitext(question_file)[0]
+            document_id_index = question_main_filename.find('-questions')
+            assert document_id_index != -1, 'FairytaleQA filenames parsing error'
+            document_id = question_main_filename[:document_id_index]
+
+            context_file = document_id + '-story.csv'
+
+            question_filename = os.path.join(question_directory, question_file)
+            context_filename = os.path.join(context_directory, context_file)
+ 
+            with open(question_filename) as qf, open(context_filename) as cf:
+                qcsv, ccsv = csv.reader(qf), csv.reader(cf)
+                qheader, cheader = next(qcsv), next(ccsv)
+                
+                clist = list(ccsv) # list of <section_id(str), its text(str)> pairs
+                whole_story = ' '.join(map(lambda x: x, ccsv))
+                
+                entry_id_index, section_id_in_context_index, reasoning_skill_index = qheader.index('question_id'), qheader.index('cor_section'), qheader.index('attribute1')
+
+                for qentry in qcsv:
+                    '''
+                    entry fields:
+                    [ (0) 'question_id', (1) 'local-or-sum', (2) 'cor_section', (3) 'attribute1', (4) 'attribute2', (5) 'question', (6) 'ex-or-im1', (7) 'answer1', (8) 'answer2', (9) 'answer3', (10) 'ex-or-im2', (11) 'answer4', (12) 'answer5', (13) 'answer6']
+                    '''
+                    entry_id, reasoning_skill = qentry[entry_id_index], qentry[reasoning_skill_index]
+                    section_id_in_context = qentry[section_id_in_context_index]
+                    context = section2context(section_id_in_context, clist)
+                    qid = document_id + '-' + entry_id
+
+                    self.contexts.append(context)
+                    self.questions.append(question)
+                    self.answers.append(answer)
+                    self.reasoning_skills.append(reasoning_skill)
+                    input_context = cxt_token + ' ' + context + ' ' + \
+                                    ans_token + ' ' + answer + ' ' + \
+                                    rsk_token + ' ' + reasoning_skill
+                    
+                    input_contexts.append(input_context)
+                    self.qids.append(qid)
+        '''
+        convert the whole of dataset into torch.*Tensor (tensor {tensor from constant to scalar}), cache them in the CPU RAM, and feed a mini-batch of samples into GPU memory when necessary
+        '''
+        if self.split_set in { 'train', 'val' }:
+            train_inputs, test_inputs = self.prepare_input(input_contexts, self.questions)
+        else:
+            train_inputs, test_inputs = self.prepare_input(input_contexts)
+
+        self.train_inputs, self.test_inputs = train_inputs, test_inputs
+
+    def __getitem__(self, index):
+        train_input = { }
+        test_input = { }
+        question_text = self.questions[index]
+        qid = self.qids[index]
+
+        if self.split_set in { 'train', 'val' }:
+            for key in self.train_inputs.keys():
+                train_input[key] = self.train_inputs[key][index]
+            for key in self.test_inputs.keys():
+                test_input[key] = self.test_inputs[key][index]
+
+            if self.split_set == 'train':
+                return train_input, test_input, question_text
+            elif self.split_set == 'val':
+                return train_input, test_input, question_text, qid
+        else:
+            for key in self.test_inputs.keys():
+                test_input[key] = self.test_inputs[key][index]
+
+            # fake test in our experiments, because they contain the ground truth sequence
+            return test_input, question_text, qid
+
+    def __len__(self):
+        return len(self.qids)
---
+++ skillqgpackage/model/BertForMultiLabelSequenceClassification.py
@@ -0,0 +1,77 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from torch.nn import init
+from transformers import BertPreTrainedModel, BertModel
+from transformers.modeling_outputs import SequenceClassifierOutput
+
+'''
+    Huggingface Transformers v4.4.2 only
+'''
+class BertForMultiLabelSequenceClassification(BertPreTrainedModel):
+    def __init__(self, config):
+        super().__init__(config)
+        self.num_labels = config.num_labels
+
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+
+        self.init_weights()
+
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+
+        outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output) # B x num_labels
+
+        loss = None
+        if labels is not None:
+            assert logits.shape == labels.shape, 'Error in the labels of multilabel classification'
+            # Multi-label classification == num_labels times binary classification
+            independent_normalized_logits = F.sigmoid(logits)
+            # B(optional=1) x num_labels x (optional shape HxW, DxHxW, or L)
+            loss_fct = nn.BCELoss(reduction = 'sum')
+            '''
+            `nn.BCELoss` is typically applied to binary classification (pipeline shape: B1, B1xHxW, or B1xDxHxW).
+            Hereon, we binarily classify a label of a sample and its shape (BxC) seems vague and confusing because this shape is more common in a multi-class classification setting. However, according to the official implementation, `nn.BCELoss` will work very well and exactly calculate what we want.
+            The final loss is computed as the average of losses of samples in the batch, while the loss of a sample is the summation of all its labels.
+            '''
+            raw_loss = loss_fct(independent_normalized_logits, labels)
+            loss = raw_loss / labels.size(-1)
+
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
